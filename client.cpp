82ae8fb258e3eb52df2127b672d27fc38c2fec50
Jack <hanzhenhong215@163.com>
2023-01-05 10:17:26 +0800

    modify client

--- client.cpp
+++ client.cpp
@@ -3,9 +3,12 @@
 
 using namespace std;
 
-const int N = 2000;
+const int N = 1000;      // number of clients
+const int tCount = 2;   // number of threads
 
 bool flag = true;
+
+EasyTcpClient *client[N];
 
 void CommandLine() {
 
@@ -20,33 +23,48 @@ void CommandLine() {
     }
 }
 
-int main() {
+void SendThread(int id) {
 
-    EasyTcpClient *client[N];
+    cout << "Thread " << id << " starts!" << endl;
 
-    for (int i = 0; i < N; i++)
+    int segment = N / tCount;
+    int begin = (id - 1) * segment;
+    int end = begin + segment;
+
+    for (int i = begin; i < end; i++)
         client[i] = new EasyTcpClient();
     
-    thread tid(CommandLine);
-    tid.detach();
-    
-    for (int i = 0; i < N; i++) {
-        if (!flag) return 0;
+    for (int i = begin; i < end; i++) 
         client[i]->Connect("127.0.0.1", 4567);
-        printf("client<%d> connected!\n", i + 1);
-    }
+    
+    printf("thread<%d> Connect <begin = %d, end = %d>\n", id, begin, end);
 
     Login login;
     strcpy(login.Name, "hzh");
     strcpy(login.Password, "123456");
     
-    while (flag) {
-        for (int i = 0; i < N; i++)
+    while (flag) 
+        for (int i = begin; i < end; i++)
             client[i]->SendData(&login);
+    
+
+    for (int i = begin; i < end; i++)
+        client[i]->Close();
+
+}
+
+int main() {
+
+    thread tid(CommandLine);
+    tid.detach();
+
+    for (int i = 1; i <= tCount; i++) {
+        thread tid(SendThread, i);
+        tid.detach();
     }
 
-    for (int i = 0; i < N; i++)
-        client[i]->Close();
+    while (flag)
+        sleep(10);
 
     return 0;
 }
